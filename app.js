11bc631ab41a4c0f5a021ed464366ad8942b5257
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-03 23:31:43 -0500

    Completed Assignment

--- app.js
+++ app.js
@@ -17,3 +17,34 @@
 // Question 5
 // const containerID = document.querySelector("#container");
 // containerID.innerText = "Hello";
+
+// Question 6
+// let divFooter = document.querySelector(".footer");
+// divFooter.classList.add("main");
+// console.log(divFooter);
+
+// Question 7
+// let divFooter = document.querySelector(".footer");
+// divFooter.classList.add("main");
+// divFooter.classList.remove("main");
+// console.log(divFooter);
+
+// Question 8,9,10
+const ul = document.querySelector("ul");
+const newLi = document.createElement("li");
+newLi.innerText = "Four";
+ul.append(newLi);
+// console.log(ul);
+
+// Question 11
+
+const listItems = document.querySelectorAll("ol li");
+console.log(listItems);
+
+for (let listItem of listItems) {
+  listItem.style.backgroundColor = "green";
+}
+
+// Question 12
+let footerDiv = document.querySelector(".footer");
+footerDiv.remove();
