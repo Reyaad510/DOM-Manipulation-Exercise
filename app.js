8fc53a67f1424a68f3b94c61f897b8c3aa9b80a8
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-03 23:07:17 -0500

    Answered first 5 questions

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("hi");
+// Question 1
+// const containerID = document.getElementById("container");
+// console.log(containerID);
+
+// Question 2
+// const containerID = document.querySelector("#container");
+// console.log(containerID);
+
+// Question 3
+// const liSeconds = document.querySelectorAll("li.second");
+// console.log(liSeconds);
+
+// Question 4
+// const olThird = document.querySelector("ol .third");
+// console.log(olThird);
+
+// Question 5
+const containerID = document.querySelector("#container");
+containerID.innerText = "Hello";
